6910a5a1c01901baba1aa1cfb092c775c280f9fb
Andrea Galliani <agalliani@latek.it>
2023-05-04 22:41:16 +0200

    i secret non vanno, per ora lascio tutto in chiaro e stica... :(

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,9 @@
-export const environment = {
-  firebase: {
-    projectId: 'website-frontend-firebase',
-    appId: '1:100730812687:web:1dc297c1e1fe548ee8267a',
-    databaseURL: 'https://website-frontend-firebase-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'website-frontend-firebase.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'undefined', //managed in the secrets
-    authDomain: 'website-frontend-firebase.firebaseapp.com',
-    messagingSenderId: '100730812687',
-    measurementId: 'G-5SC1C33J1T',
-  },
-  google_client_id: 'undefined', //managed in the secrets
-  authorized_people: [], //managed in the secrets
+import { oauth2 } from './oauth2.environment'
+import { firebase } from './firebase.environment';
 
+export const environment = {
+  ...firebase,
+  ...oauth2,
   production: true,
   name: 'microlab-unibg',
   version: '1.0.0'
---
+++ src/environments/oauth2.environment.ts
@@ -0,0 +1,3 @@
+export const oauth2 = {
+  google_client_id: '100730812687-lh1opcloap262quiioatcae8hs799skl.apps.googleusercontent.com'
+};
