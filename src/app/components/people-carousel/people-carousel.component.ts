a292c3808da411ce420459ab1b27de4fbe972226
lucaghislo <ghislottiluca@gmail.com>
2026-09-08 10:42:30 +0200

    Updated people carousel

--- src/app/components/people-carousel/people-carousel.component.ts
+++ src/app/components/people-carousel/people-carousel.component.ts
@@ -64,7 +64,7 @@ export class PeopleCarouselComponent {
       },
       {
         fullname: "Gianluca Traversi",
-        role: "Associate Professor",
+        role: "Full Professor",
         image_link: "https://aisberg.unibg.it/rm/public/picture/img/it.cilea.ga.model.Person/58da8ec3-d49a-42dc-87e9-2d63477b442e.fragment",
         page_link: 'https://didattica-rubrica.unibg.it/ugov/person/2025'
       },
